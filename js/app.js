4a5d49ff6cd7055cb103d2dbf54f57363a35c71e
ambionvra21-ship-it <ambionvra21@gmail.com>
2026-07-26 22:01:14 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,28 +5,38 @@
 
 console.log("🚀 Starting Infinity Alpha OS...");
 
+
 window.addEventListener("DOMContentLoaded", () => {
+
 
     console.log("✅ DOM Ready");
 
+
     // Greeting
     if (typeof updateGreeting === "function") {
+
         updateGreeting();
+
         console.log("👋 Greeting Ready");
+
     }
+
+
 
     // Weather
     if (window.Weather) {
+
         Weather.init();
-        console.log("🌤 Weather Ready");
+
+        console.log("🌤 Weather Started");
+
     }
 
-    // Future Modules
-    // Finance.init();
-    // Crypto.init();
-    // Stocks.init();
-    // News.init();
 
-    console.log("🤖 Infinity Alpha OS Ready");
+
+    console.log(
+        "🤖 Infinity Alpha OS Ready"
+    );
+
 
 });
